2e98f58d7a2303401303eb41c00542816f5fa561
Umair Majeed <umair.majeed5786@gmail.com>
2017-07-27 05:08:22 +0500

    updating the popup layout and removing the extra stuff.

--- app/code/local/Mage/DilverySign/Model/Observer.php
+++ app/code/local/Mage/DilverySign/Model/Observer.php
@@ -2,12 +2,65 @@
 
 class Mage_DilverySign_Model_Observer 
 {
-    public function saveDeliverySignature($event)
-    {
-		die('there');
-        $quote = $event->getSession()->getQuote();
-        $quote->setData('delivery_signature', $event->getRequestModel()->getPost('delivery_signature'));
+		public function saveQuoteBefore($evt){
 
-        return $this;
-    }
+		$quote = $evt->getQuote();
+
+		$post = Mage::app()->getFrontController()->getRequest()->getPost();
+
+		if(isset($post['delivery_signature'])){
+
+		$var = $post['delivery_signature'];
+	
+		$quote->setDeliverySignature($var);
+
+		}
+
+		}
+
+		public function saveQuoteAfter($evt){
+
+		$quote = $evt->getQuote();
+		
+		}
+
+		public function loadQuoteAfter($evt){
+
+		$quote = $evt->getQuote();
+
+		}
+
+		public function saveOrderAfter($evt){
+
+		$order = $evt->getOrder();
+
+		$quote = $evt->getQuote();	
+
+		$post = Mage::app()->getFrontController()->getRequest()->getPost();
+
+		if(!empty($quote->getDeliverySignature())){
+			$var = $quote->getDeliverySignature();
+		$order->setDeliverySignature($var);
+
+		}
+		
+		if(empty($order->getShippingAddress()->getTelephone())){
+			$shippingAddress = Mage::getModel('sales/order_address')->load($order->getShippingAddress()->getId());
+			$shippingAddress->setTelephone("801-701-9446");
+			
+		}
+		if(empty($order->getBillingAddress()->getTelephone())){
+			$billingAddress = Mage::getModel('sales/order_address')->load($order->getBillingAddress()->getId());
+			$billingAddress->setTelephone("801-701-9446");
+		}
+
+		
+
+		}
+
+		public function loadOrderAfter($evt){
+
+		$order = $evt->getOrder();
+
+		}
 }
